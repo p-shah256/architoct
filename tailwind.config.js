66c05c57cab6ac717117d0f5a6b5ed2d42bef660
Pranchal Shah <pranchx@gmail.com>
2024-11-22 17:46:20 -0500

    tailwind locally works use with nix shell

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./views/**/*.{html,js,templ,gohtml}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
